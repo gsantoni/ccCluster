58fea6d6014ebd5bfd0888e5152a8acffd9af4c8
gsantoni <santonigl@gmail.com>
2019-12-23 13:29:29 +0100

    added placeholder for setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+__author__ = "Gianluca Santoni"
+__copyright__ = "Copyright 20150-2019"
+__credits__ = ["Gianluca Santoni, Alexander Popov"]
+__license__ = ""
+__version__ = "1.0"
+__maintainer__ = "Gianluca Santoni"
+__email__ = "gianluca.santoni@esrf.fr"
+__status__ = "Beta"
